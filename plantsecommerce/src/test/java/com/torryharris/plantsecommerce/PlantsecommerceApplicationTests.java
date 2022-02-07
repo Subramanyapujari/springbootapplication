f7c9c4f71a4819fb0472883e7f2e43c389396020
Subramanyapujari <75833089+Subramanyapujari@users.noreply.github.com>
2022-02-07 18:06:08 +0530

    Add files via upload

---
+++ plantsecommerce/src/test/java/com/torryharris/plantsecommerce/PlantsecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torryharris.plantsecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
